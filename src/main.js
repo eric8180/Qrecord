99361fa247d392d3a36e41139ce3ad56835a6fba
eric8180 <357424562@qq.com>
2019-08-19 17:27:08 +0800

    Signed-off-by: eric8180 <357424562@qq.com>

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import elementui from 'element-ui'
-
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 
 new Vue({
