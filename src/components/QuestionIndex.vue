e5e11e9fd2323f440994b276600151241cae10e1
eric8180 <357424562@qq.com>
2019-08-21 10:30:22 +0800

    done
    done

---
+++ src/components/QuestionIndex.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="home">
+    <div id="img">
+        <img alt="Question logo" src="../assets/question.jpg">
+    </div>
+    <div id="question">
+      <ul>
+        <li><router-link to="/judge">"我的"页面中显示的姓名不正确</router-link></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuestionIndex'
+}
+</script>
+
+<style>
+#home {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#question a{
+  text-decoration: none;
+  color:cornflowerblue
+}
--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 //组件模块
+import QuestionHome from './components/QuestionIndex.vue'
 import judge from './components/judge.vue';
 import Notid from './components/Notid.vue'; 
 import Isid from './components/Isid.vue';
@@ -15,7 +16,8 @@ Vue.use(Router)
 
 export default new Router({
     routes:[
-        {path:'/',component:judge},
+        {path:'/',component:QuestionHome},
+        {path:'/judge',component:judge},
         {path:'/Notid',component:Notid},
         {path:'/Isid',component:Isid},
         {path:'/Idright',component:Idright},
