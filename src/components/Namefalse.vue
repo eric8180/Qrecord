b80cef34391130b0ee7ee90bbcbe90152bbe92b9
fan <fanyf628@163.com>
2019-08-21 11:17:28 +0800

    Signed-off-by: fan <fanyf628@163.com>

---
+++ src/components/Namefalse.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="Namefalse">
+    
+      <h1>修改保单上的姓名</h1>
+      
+      <ul>
+      <el-row>
+      
+      <el-button><router-link to ="./">返回首页</router-link></el-button>
+      </el-row>
+      </ul>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Namefalse'
+}
+</script>
+
+<style>
+</style>
+
